3be26ddd037b57beb387946d3ac592aa82a98035
LukeNicklin <luke.nicklin@digital.homeoffice.gov.uk>
2024-04-12 16:08:06 +0100

    New screens for ACRS

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,12 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/evidence-answer', function(request, response) {
+
+    var evidence = request.session.data['evidence']
+    if (evidence.includes("There is evidence I cannot provide")){
+        response.redirect("/there-is-evidence-i-cannot-provide")
+    } else {
+        response.redirect("/where-should-we-send-your-decision")
+    }
+})
