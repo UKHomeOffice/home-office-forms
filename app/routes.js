8978b08f97e127ccb6178d8170a4920f23ac7850
LukeNicklin <luke.nicklin@digital.homeoffice.gov.uk>
2024-04-18 16:04:34 +0100

    Over 18 Scenario

--- app/routes.js
+++ app/routes.js
@@ -21,7 +21,7 @@ router.post('/who', function(request, response) {
 
     var who = request.session.data['who']
     if (who == "The applicant"){
-        response.redirect("/acrs/what-is-your-full-name")
+        response.redirect("/acrs/over-18/what-is-your-full-name")
     } else if (req.session.data['who'] === 'Someone helping the applicant') {
         response.redirect("")
     }
@@ -34,3 +34,11 @@ router.all('/afm-router', function (req, res, next) {
     } else {
       return (res.redirect('/acrs/under-18/family-you-want-to-apply-to-bring-to-the-uk-3'))
     }})
+
+    router.all('/afm-router-2', function (req, res, next) {
+        if ((req.session.data['birth-year-2'] >= 2007)) {
+          return res.redirect('/acrs/over-18/who-is-responsible-for-this-child');
+          next();
+        } else {
+          return (res.redirect('/acrs/over-18/family-you-want-to-apply-to-bring-to-the-uk-3'))
+        }})
